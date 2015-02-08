1bb3f25bd6fb09adbfdebb543b5df1d4d056891c
Maikel Zweerink <maikeldus@hotmail.com>
2015-02-08 11:59:54 +0100

    Moved DB creation out of the script

--- api/whatsspy-db.sql
+++ api/whatsspy-db.sql
@@ -1,17 +1,3 @@
-CREATE ROLE whatsspy LOGIN
-  PASSWORD ''
-  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
-
-CREATE DATABASE whatsspy
-  WITH OWNER = whatsspy
-       ENCODING = 'UTF8'
-       TABLESPACE = pg_default
-       LC_COLLATE = 'en_US.UTF-8'
-       LC_CTYPE = 'en_US.UTF-8'
-       CONNECTION LIMIT = -1;
-GRANT ALL ON DATABASE whatsspy TO whatsspy;
-
-
 -- Sequence: status_sid_seq
 
 -- DROP SEQUENCE status_sid_seq;
