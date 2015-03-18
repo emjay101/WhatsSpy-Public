83fd2f59ec600a04cbcc2c72c0104c65e0955cf6
Maikel Zweerink <maikeldus@hotmail.com>
2015-03-18 22:53:12 +0100

    Up version

--- js/app.js
+++ js/app.js
@@ -32,7 +32,7 @@ angular.module('whatsspy', ['ngRoute', 'ngVis', 'whatsspyFilters', 'whatsspyCont
 })
 .controller('MainController', function($scope, $rootScope, $location, $http, $q, $filter) {
   // Version of the application
-  $rootScope.version = '1.4.1';
+  $rootScope.version = '1.4.2';
 
   $('[data-toggle="tooltip"]').tooltip();
 
